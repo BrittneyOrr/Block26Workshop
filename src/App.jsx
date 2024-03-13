5e0185f744ef84abcc3bbcc481e0d3670e678d88
Sequoia Nevae <sequoianevae@Sequoias-iMac.lan>
2024-03-12 22:13:28 -0500

    finished Setting up the Table Tab

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { useState } from 'react'
 import './App.css'
-import './ContactList' from 'components'  
+import ContactList from './components/ContactList.jsx'
 
 export default function App() {
-  // const [count, setCount] = useState(0)
 
   return (
    <>
-   <ContactList />
+    <ContactList />
    </>
   );
 }
